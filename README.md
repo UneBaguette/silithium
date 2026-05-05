4b1929e4fbac50919029e3d6ced14d5c99de7fc5
UneBaguette <28904802+UneBaguette@users.noreply.github.com>
2026-05-06 00:19:21 +0200

    feat: lib, added basic function
    - Still work in progress, gonna look into it later!

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# silithium
-Silithium is a hybrid signature combining EC-Schnorr (or ECDSA) with ML-DSA
+# Silithium
+
+Silithium is a hybrid signature combining EC-Schnorr (or ECDSA) with ML-DSA.
+
+The current variant is only the **silithium-44**.
+
+## Resources
+
+This is based on the recent research paper [Compact, Efficient and Non-Separable Hybrid Signatures](https://ia.cr/2025/2059).
+
+## License
+
+This projet is dual-licensed under the [MIT License](LICENSE-MIT) or [Apache 2.0 License](LICENSE-APACHE).
---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// Copyright 2026 Thomas <tom@unebaguette.fr>
+
+use ml_dsa::KeyGen;
+use p256::elliptic_curve::Generate;
+use getrandom::{SysRng, rand_core::UnwrapErr};
+use ml_dsa::signature::Keypair;
+use p256::elliptic_curve::sec1::ToSec1Point;
+use sha3::digest::{ExtendableOutput, Update, XofReader};
+
+pub struct SigningKey {
+    // EC-Schnorr
+    sk1: p256::SecretKey,
+    vk1: p256::PublicKey,
+    // ML-DSA
+    sk2: ml_dsa::ExpandedSigningKey<ml_dsa::MlDsa44>,
+    vk2: ml_dsa::VerifyingKey<ml_dsa::MlDsa44>,
+    // Precomputed
+    tr: [u8; 64],
+}
+
+pub struct VerifyingKey {
+    vk1: p256::PublicKey,
+    vk2: ml_dsa::VerifyingKey<ml_dsa::MlDsa44>,
+    tr: [u8; 64],
+}
+
+pub struct Signature {
+    ml_dsa_sig: ml_dsa::Signature<ml_dsa::MlDsa44>,
+    x: p256::Scalar,
+}
+
+pub fn generate_keypair() -> SigningKey {
+    let sk1 = p256::SecretKey::generate();
+    let vk1 = sk1.public_key();
+
+    let ml_dsa_kp = ml_dsa::MlDsa44::key_gen(&mut UnwrapErr(SysRng));
+
+    let sk2 = ml_dsa_kp.signing_key().clone();
+    let vk2 = ml_dsa_kp.verifying_key();
+
+    let vk1_bytes = vk1.to_sec1_point(false);
+    let vk2_bytes = vk2.encode();
+
+    let vk1_result = vk1_bytes.as_bytes();
+
+    let mut tr = sha3::Shake256::default();
+
+    tr.update(&vk1_result);
+    tr.update(&vk2_bytes);
+
+    let mut tr_reader = tr.finalize_xof();
+
+    let mut tr = [0u8; 64];
+
+    tr_reader.read(tr.as_mut());
+
+    SigningKey {
+        sk1,
+        vk1,
+        sk2,
+        vk2,
+        tr
+    }
+}
