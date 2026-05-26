55c3bcee75ff49937efaf453e78af6855a473942
UneBaguette <28904802+UneBaguette@users.noreply.github.com>
2026-05-26 14:18:06 +0200

    feat: switch to shake crate instead of sha3, update ml-dsa and added features for possible ecdsa

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 // Copyright 2026 Thomas <tom@unebaguette.fr>
 
-use ml_dsa::KeyGen;
-use p256::elliptic_curve::Generate;
+use ml_dsa::Generate;
 use getrandom::{SysRng, rand_core::UnwrapErr};
 use ml_dsa::signature::Keypair;
 use p256::elliptic_curve::sec1::ToSec1Point;
-use sha3::digest::{ExtendableOutput, Update, XofReader};
+use shake::digest::{ExtendableOutput, Update, XofReader};
 
 pub struct SigningKey {
     // EC-Schnorr
@@ -34,17 +33,17 @@ pub fn generate_keypair() -> SigningKey {
     let sk1 = p256::SecretKey::generate();
     let vk1 = sk1.public_key();
 
-    let ml_dsa_kp = ml_dsa::MlDsa44::key_gen(&mut UnwrapErr(SysRng));
+    let key: ml_dsa::SigningKey<ml_dsa::MlDsa44> = Generate::generate_from_rng(&mut UnwrapErr(SysRng));
 
-    let sk2 = ml_dsa_kp.signing_key().clone();
-    let vk2 = ml_dsa_kp.verifying_key();
+    let sk2= key.expanded_key().clone();
+    let vk2 = key.verifying_key();
 
     let vk1_bytes = vk1.to_sec1_point(false);
     let vk2_bytes = vk2.encode();
 
     let vk1_result = vk1_bytes.as_bytes();
 
-    let mut tr = sha3::Shake256::default();
+    let mut tr = shake::Shake256::default();
 
     tr.update(&vk1_result);
     tr.update(&vk2_bytes);
